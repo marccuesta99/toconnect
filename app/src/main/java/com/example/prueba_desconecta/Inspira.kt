b8d623c3090eac66aeed45e8a68857a395191026
andreastourino <andreas.tourinoo@gmail.com>
2020-10-21 18:31:33 +0200

    version2.0

---
+++ app/src/main/java/com/example/prueba_desconecta/Inspira.kt
@@ -0,0 +1,11 @@
+package com.example.prueba_desconecta
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class Inspira : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_inspira)
+    }
+}
