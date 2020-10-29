105e36507439efd9248cce971e4e92d510158a40
andreastourino <andreas.tourinoo@gmail.com>
2020-10-29 11:42:37 +0100

    version2.1

---
+++ app/src/main/java/com/example/prueba_desconecta/Foto.kt
@@ -0,0 +1,24 @@
+package com.example.prueba_desconecta
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class Foto : AppCompatActivity() {
+
+    val REQUEST_IMAGE_CAPTURE = 1
+
+
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_foto)
+    }
+    private fun dispatchTakePictureIntent() {
+            Intent(MediaStore.ACTION_IMAGE_CAPTURE).also { takePictureIntent ->
+            takePictureIntent.resolveActivity(packageManager)?.also {
+                startActivityForResult(takePictureIntent, REQUEST_IMAGE_CAPTURE)
+            }
+        }
+    }
+}
